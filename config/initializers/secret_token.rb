f486fac637b58fccfe24b4635b454608057e24bf
Malik Miller <malikilam@hotmail.com>
2013-01-28 11:21:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyPawn2::Application.config.secret_token = 'be34ec53f67fd5e4482224d3b7331f5ea5fdf1ff4a24f26421df4840688da96ef6795abafd8b8f9db6ec529ff02de33320a64cb44a8209d4a6aaec9aa35173c5'
